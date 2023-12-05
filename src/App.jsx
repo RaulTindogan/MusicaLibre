2f40882c3060e53668c1931327632b90e7b7c726
RaulTindogan <tindoganraul@gmail.com>
2023-12-05 10:46:59 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         <main>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path='/discover' element={<Discover />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<ContactPage />} />
